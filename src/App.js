7151647b063384f167762129608ef37a47f1750d
Waavy <benarr91@naver.com>
2022-02-24 19:29:47 +0900

    #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '장승배기 고기 맛집';
+  let ind = { color : 'almond', fontSize : '24px' };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Nav-bar */}
+      <div className="black-nav">
+        <div style={ ind }>Benkim Velog</div>
+      </div>
+      <h4> { posts } </h4>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+< JSX 문법 >
+  1) react에서는 class=" " 대신 className=" " 사용하기
+  2) { }열어서 데이터바인딩 하기
+    - 선언된 변수를 사용하려면 사용하고 싶은 위치에서 중괄호 열어 사용하면 됨
+
+*/
